e00dbcb58acba06d82bfca9b7b130e1bcfe8f0bf
hamishgibbs <hamishgibbs@gmail.com>
2020-12-02 12:41:28 +0000

    update tests

--- tests/processing/test_processing_utils.py
+++ tests/processing/test_processing_utils.py
@@ -385,3 +385,31 @@ class Test_remove_tags:
         record = utils.remove_tags(record, ['comments'])
 
         assert record['comments'] is None
+
+class Test_replace_country:
+
+    def test_replace_country_present(self):
+
+        country_name = 'Denmark'
+        area_name = 'Greenland'
+
+        record = {'country_territory_area':country_name,
+                  'area_covered':area_name}
+
+        res = utils.replace_country(record, country_name, area_name)
+
+        assert res['country_territory_area'] == area_name
+
+        assert res['area_covered'] is None
+
+    def test_replace_country_absent(self):
+
+        country_name = 'Denmark'
+        area_name = 'Greenland'
+
+        record = {'country_territory_area':country_name,
+                  'area_covered':area_name}
+
+        res = utils.replace_country(record, country_name, 'Something else')
+
+        assert res['country_territory_area'] == country_name
